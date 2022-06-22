0c2f0daeea7bab9393fc01a2acf2f2f5a1ed2d30
jabed366 <jabedhasan231@gmail.com>
2022-06-23 03:28:16 +0600

    Binary search Js

--- .vscode/binary_search/binary_search.js
+++ .vscode/binary_search/binary_search.js
@@ -1,17 +1,26 @@
+//array elements
+
 let array=[15,26,37,48,69];
 
+//find the array element
 let find=48;
 
+//init variable 
 let i=0;
-
-let beg=0;
 let count=0;
+
+//find array element
+let beg=0;
+
+//last array element
 let end=array.length-1;
+
+
 let mid;
 while(beg<=end) {
-   
+   //changing mid point avery loop
     mid= parseInt( (beg+end)/2);
-   
+   //condition check mid the the targeted value or not
     if(array[mid]==find){
         console.log("number found the position: " + (mid+1) );
         count++;
@@ -27,6 +36,7 @@ while(beg<=end) {
 
 }
 
+//position are found
 if(count!=1){
     console.log("number is not found");
 }
